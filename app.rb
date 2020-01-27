47fb6b509416ae43124652ab9d627c195f1c02ff
Jeff Swisher <jefftswisher@gmail.com>
2020-01-27 21:43:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,14 @@ class App < Sinatra::Base
   end 
   
   post '/team' do 
-    @team = Team.new 
-    @team.name = params[:name]
-    @team.coach = params[:coach]
-    @team.pg = params[:pg]
-    @team.sg = params[:sg]
-    @team.pf = params[:pf]
-    @team.sf = params[:sf]
-    @team.c = params[:c]
-    @team.save
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+  
     erb :team 
   end 
 
